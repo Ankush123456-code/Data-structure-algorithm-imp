aa9af24d0bddbc7aaf7213867855340d58e95430
ankushkumar1840 <ankushkumar1840@gmail.com>
2022-09-24 14:29:29 +0530

    leetcode_tree

---
+++ Using_Java/leetcode_java_tree/path_sum_1.java
@@ -0,0 +1,22 @@
+package Using_Java.leetcode_java_tree;
+
+public class path_sum_1 {
+
+    private static boolean dfs(TreeNode root, int t) {
+        if (root == null) {
+            return false;
+        }
+        t = t - root.val;
+        if (root.left == null && root.right == null) {
+            if (t == 0) {
+                return true;
+            }
+        }
+        return dfs(root.left, t) || dfs(root.right, t);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        return dfs(root, targetSum);
+    }
+}
