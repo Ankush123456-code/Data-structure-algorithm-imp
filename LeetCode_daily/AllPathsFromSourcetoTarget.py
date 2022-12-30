0179982b08aa2af69ddeea8f524a0b122ea74acf
ankushkumar1840 <ankushkumar1840@gmail.com>
2022-12-30 14:47:47 +0530

    allPathsSourceTarget

---
+++ LeetCode_daily/AllPathsFromSourcetoTarget.py
@@ -0,0 +1,22 @@
+class Solution:
+    def allPathsSourceTarget(self, graph):
+        ans = []
+        temp = []
+
+        def dfs(src, dest):
+            temp.append(src)
+            if src == dest:
+                ans.append(list(temp))
+            for i in graph[src]:
+                dfs(i, dest)
+            temp.pop()
+
+        dfs(0, len(graph) - 1)
+        return ans
+
+
+if __name__ == '__main__':
+    graph = [[1, 2], [3], [3], []]
+    obj = Solution()
+    res = obj.allPathsSourceTarget(graph)
+    print(res)
